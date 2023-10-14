5fcf4ad0680989fe22dd9381604ae4452619a551
Officialdeemmy <adenijiademolaafis@gmail.com>
2023-10-14 02:17:11 -0700

    0

--- main.h
+++ main.h
@@ -1,6 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
+
 int _putchar(char c);
 int _printf(const char *format, ...);
 int printf_char(va_list va);
