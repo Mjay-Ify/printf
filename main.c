273a3737ce32a415e907765581baf6ca97131c80
Officialdeemmy <adenijiademolaafis@gmail.com>
2023-10-17 19:42:28 -0700

    trying

--- main.c
+++ main.c
@@ -8,7 +8,7 @@
  *
  * Return: Always 0
  */
-/**
+
 
 int main(void)
 {
@@ -42,11 +42,11 @@ int main(void)
     _printf("Len:[%d]\n", len);
     printf("Len:[%d]\n", len2);
     _printf("Unknown:[%%r]\n");
-    printf("Unknown:[%r]\n");
+   /* printf("Unknown:[%r]\n");*/
     return (0);
 }
 
-*/
+
 
 /**
  * main - Entry point
@@ -54,6 +54,7 @@ int main(void)
  * Return: Always 0
  */
 
+/**
 
 int main(void)
 {
@@ -115,4 +116,5 @@ int main(void)
 	return (0);
 }
 
+*/
 
